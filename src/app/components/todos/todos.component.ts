b2bb9a45f349a324cda78008d8033c20abdd6b9b
yacineessid <yacine.essid@gmail.com>
2024-04-23 18:52:44 +0100

    save todos in localstorage

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,37 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import {Todo }from './../../models/Todo'
+import { Todo } from './../../models/Todo';
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-todos:Todo[]
-inputTodo:string="";;
+  todos: Todo[];
+  inputTodo: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
-    this.todos=[]
+    this.todos = JSON.parse(localStorage.getItem('todos') || '[]');
   }
-toggleDone(id:number){
-this.todos.map((v ,i)=>{
-if(i==id) v.completed =!v.completed;
-return v;
-})
-}
-deleteTodo(id:number){
-  if (confirm("Êtes-vous sûr de vouloir supprimer ce to-do list ?")){
-    this.todos=this.todos.filter((v,i)=>i !==id);
 
+  toggleDone(id: number) {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+      return v;
+    });
+    this.updateLocalStorage();
   }
-}
-addTodo(){
-  if(this.inputTodo.length)
-  this.todos.push({
-    content:this.inputTodo,
-    completed:false
-  })
-  this.inputTodo="";
-}
 
+  deleteTodo(id: number) {
+    if (confirm("Are you sure you want to delete this todo?")) {
+      this.todos = this.todos.filter((v, i) => i !== id);
+      this.updateLocalStorage();
+    }
+  }
+
+  addTodo() {
+    if (this.inputTodo.length) {
+      this.todos.push({
+        content: this.inputTodo,
+        completed: false
+      });
+      this.updateLocalStorage();
+    }
+    this.inputTodo = "";
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
 }
